7e78aa39a9ef5730748fb23b66053aabe80f1148
ajanmcr <60147219+ajanmcr@users.noreply.github.com>
2020-03-01 18:23:26 +0530

    Committing Collection Hometask

---
+++ collection/custom_list/src/main/java/com/collections/Clist/custom_list/Main.java
@@ -0,0 +1,23 @@
+package com.collections.Clist.custom_list;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DataList<Integer> list = new DataList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+
+        list.remove(2);
+        System.out.println(list);
+
+        System.out.println( list.get(0) );
+        System.out.println( list.get(1) );
+
+        System.out.println(list.size());
+	}
+
+}
